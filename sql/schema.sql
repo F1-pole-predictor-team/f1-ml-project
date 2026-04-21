27363505f18617084af90bc56e5fad892ed7ef0a
ajkenek <ajken2115@gmail.com>
2026-04-21 12:05:04 +0200

    feat: adding first basic table scheme

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS raw_laps (
+    Driver TEXT,
+    LapTime FLOAT,
+    S1_time FLOAT,
+    S2_time FLOAT,
+    S3_time FLOAT,
+    Compound TEXT,
+    Tyre_life FLOAT,
+    Event_type TEXT,
+    Event_name TEXT
+);
