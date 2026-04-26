a57e6dbef50df4c50b630cda7c0983e1d9286531
Edyta Pietrzak <edpiet19@gmail.com>
2026-04-26 21:20:07 +0200

    Dodano slownik TRACK_TYPES dla sezonu 2024/2025

---
+++ config/track_config.py
@@ -0,0 +1,34 @@
+# Słownik kategoryzujący tory F1 na podstawie ich charakterystyki
+# Używany do Feature Engineeringu w modelu ML
+
+TRACK_TYPES = {
+    # Tory uliczne (Street)
+    'Monaco': 'street',
+    'Singapore': 'street',
+    'Las Vegas': 'street',
+    'Baku': 'street',
+    'Miami': 'street',
+
+    # Tory bardzo szybkie (High Speed)
+    'Monza': 'high_speed',
+    'Spa-Francorchamps': 'high_speed',
+    'Jeddah': 'high_speed',
+
+    # Tory wymagające dużego docisku (High Aero)
+    'Silverstone': 'high_aero',
+    'Suzuka': 'high_aero',
+    'Zandvoort': 'high_aero',
+    'Barcelona': 'high_aero',
+    'Lusail': 'high_aero',
+
+    # Tory techniczne/zbalansowane
+    'Bahrain': 'technical',
+    'Hungaroring': 'technical',
+    'Interlagos': 'technical',
+    'Austin': 'technical',
+    'Abu Dhabi': 'technical',
+    'Imola': 'technical',
+    'Montreal': 'technical',
+    'Melbourne': 'technical',
+    'Spielberg': 'technical'
+}
