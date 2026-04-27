c56ef727381fb07ddc9e19fc55ee0640d85583bc
ajkenek <ajken2115@gmail.com>
2026-04-27 20:03:31 +0200

    fixes + final position from sessions

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,24 +1,18 @@
 import time
 import fastf1
 import pandas as pd
-from database_utils import get_db_engine
+from database_utils import get_db_engine, setup_fastf1_cache
 from pathlib import Path
 from sqlalchemy import text
 
 
-def download_all_20242025sessions():
-    project_root = Path(__file__).parent.parent
-    cache_path = project_root / 'data' / 'cache'
-    cache_path.mkdir(parents=True, exist_ok=True)
-    fastf1.Cache.enable_cache(cache_path.as_posix())
+def download_all_sessions():
+    setup_fastf1_cache()
 
     print("Resetowanie tabeli raw_laps w bazie danych...")
     engine = get_db_engine()
-    with engine.connect() as con:
-        con.execute(text("DROP TABLE IF EXISTS raw_laps"))
-        con.commit()
 
-    seasons = [2024, 2025]
+    seasons = [2022, 2023, 2024, 2025]
     for year in seasons:
         print(f"Pobieranie kalendarza na sezon {year}...")
         schedule = fastf1.get_event_schedule(year)
@@ -27,13 +21,16 @@ def download_all_20242025sessions():
         sessions_to_get = ['FP1', 'FP2', 'FP3', 'Q']
         for index, row in races.iterrows():
             event_name = row['EventName']
-            print(f"\n--- Przetwarzanie weekendu: {year} - {event_name} ---")
+            print(f"--- Przetwarzanie weekendu: {year} - {event_name} ---")
 
             for session_type in sessions_to_get:
                 try:
                     print(f"  -> Pobieranie sesji: {session_type}...")
                     session = fastf1.get_session(year, event_name, session_type)
                     session.load()
+
+                    results = session.results[["Abbreviation", "Position"]].copy()
+                    results = results.rename(columns={"Position": "FinalPosition", "Abbreviation": "Driver"})
 
                     laps = session.laps.pick_quicklaps()
                     dane_pogodowe = laps.get_weather_data()
@@ -42,42 +39,38 @@ def download_all_20242025sessions():
                         print(f"  Brak szybkich okrążeń dla {event_name} w sesji {session_type}. Pomijam.")
                         continue
 
-                    # ZMIANA 1: Dodanie 'SpeedST' do listy pobieranych kolumn
                     df_laps = laps[
                         ['Driver', 'LapTime', 'Sector1Time', 'Sector2Time', 'Sector3Time', 'Compound', 'TyreLife',
                          'SpeedST']
                     ].copy()
 
+                    df_laps = pd.merge(df_laps, results, on="Driver", how="left")
+
                     df_laps['EventName'] = event_name
                     df_laps['SessionType'] = session_type
-
-                    # ZMIANA 2: Dodanie Pory dnia (Data i godzina sesji)
                     df_laps['SessionDate'] = session.date
-
                     df_laps['TrackTemp'] = dane_pogodowe['TrackTemp'].values
                     df_laps['Humidity'] = dane_pogodowe['Humidity'].values
                     df_laps['AirTemp'] = dane_pogodowe['AirTemp'].values
                     df_laps['WindSpeed'] = dane_pogodowe['WindSpeed'].values
                     df_laps['WindDirection'] = dane_pogodowe['WindDirection'].values
                     df_laps['Rainfall'] = dane_pogodowe['Rainfall'].values
-
-                    # ZMIANA 3: Dodanie ciśnienia (Air Pressure)
                     df_laps['AirPressure'] = dane_pogodowe['Pressure'].values
 
                     time_columns = ['LapTime', 'Sector1Time', 'Sector2Time', 'Sector3Time']
                     for col in time_columns:
                         df_laps[col] = df_laps[col].dt.total_seconds()
 
-                    df_laps.to_sql('raw_laps', con=engine, if_exists='append', index=False)
+                    df_laps.to_sql('raw_laps', con=engine, if_exists='replace', index=False)
                     print(f" Zapisano sesję {session_type} do bazy.")
 
-                    time.sleep(2)
+                    time.sleep(4)
 
                 except Exception as e:
                     print(f"   Błąd (pomijam tę sesję): {e}")
 
-    print("\n🏆 Pobieranie danych z całego sezonu zakończone!")
+    print("🏆 Pobieranie danych z całego sezonu zakończone!")
 
 
 if __name__ == "__main__":
-    download_all_20242025sessions()
+    download_all_sessions()
