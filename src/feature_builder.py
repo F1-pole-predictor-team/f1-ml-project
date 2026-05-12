e67a61911071496bfde7cbfcb04f56d96251e5f2
ajkenek <ajken2115@gmail.com>
2026-05-13 01:41:46 +0200

    added rolling q form, pivot table and small fixes

--- src/feature_builder.py
+++ src/feature_builder.py
@@ -1,10 +1,5 @@
 import pandas as pd
-import numpy as np
-from fastf1.ergast.structure import Driver
-from pandas import DataFrame
-
 from database_utils import get_db_engine
-from sqlalchemy import text
 from track_config import TRACK_TYPES
 
 
@@ -17,13 +12,19 @@ def big_table():
 
     #  Czy to weekend sprinterski
     raw_laps['IsSprint'] = raw_laps['SessionType'].isin(sprint_sessions).astype(int)
+    raw_laps['IsSprint'] = raw_laps.groupby(['Year', 'EventName'])['IsSprint'].transform('max')
+
+    # najszybszy czas kazdego kierowcy w sesji
+    raw_laps['DriverBestLap'] = raw_laps.groupby(['Year', 'EventName', 'SessionType', 'Driver'])['LapTime'].transform('min')
+
     # Obliczamy najszybszy czas dla każdego wyścigu/sesji
     raw_laps['MinLapTime'] = raw_laps.groupby(['Year', 'EventName', 'SessionType'])['LapTime'].transform('min')
-    raw_laps['SessionRank'] = raw_laps.groupby(['Year', 'EventName', 'SessionType'])['MinLapTime'].rank(method='min')
-    raw_laps['DeltaToLeader'] = raw_laps['LapTime'] - raw_laps['MinLapTime']
+    raw_laps['SessionRank'] = raw_laps.groupby(['Year', 'EventName', 'SessionType'])['DriverBestLap'].rank(method='dense')
+    raw_laps['DeltaToLeader'] = raw_laps['DriverBestLap'] - raw_laps['MinLapTime']
+
     # Najpierw szukamy najszybszego czasu wewnątrz każdego zespołu w danej sesji
     raw_laps['MinTeamTime'] = raw_laps.groupby(['Year','EventName','SessionType','TeamName'])['LapTime'].transform('min')
-    raw_laps['DeltaToTeammate'] = raw_laps['LapTime'] - raw_laps['MinTeamTime']
+    raw_laps['DeltaToTeammate'] = raw_laps['DriverBestLap'] - raw_laps['MinTeamTime']
 
     # mapowanie słownika
     raw_laps['TrackType'] = raw_laps['EventName'].map(TRACK_TYPES)
@@ -36,23 +37,45 @@ def big_table():
     raw_laps['Best_S2'] = raw_laps.groupby(group_driver)['Sector2Time'].transform('min')
     raw_laps['Best_S3'] = raw_laps.groupby(group_driver)['Sector3Time'].transform('min')
     # najszybsze teoretyczne kółko kierowcy w danej sesji
-    raw_laps['TheoDriverBest'] = raw_laps['Best_S1'] + raw_laps['Best_S2'] + raw_laps['Best_S3']
+    raw_laps['DriverTheoBest'] = raw_laps['Best_S1'] + raw_laps['Best_S2'] + raw_laps['Best_S3']
     # najszybsze teoretyczne kółko w danej sesji
     raw_laps['TheoSessionBest'] = raw_laps.groupby(['Year', 'EventName', 'SessionType'])['DriverTheoBest'].transform('min')
-    raw_laps['TheoreticalDelta'] = raw_laps['TheoSessionBest'] - raw_laps['TheoDriverBest']
+    # delta do najszybszego kółka w sesji
+    raw_laps['TheoreticalDelta'] = raw_laps['DriverTheoBest'] - raw_laps['TheoSessionBest']
+
+    # sortowanie by najszybsze okrążenia były u góry
+    raw_laps = raw_laps.sort_values(['Year', 'EventName', 'Driver', 'SessionType', 'SessionRank'])
+    # usunięcie duplikatów = zostają tylko najlepsze okrążenia każdego zawodnika z każdej sesji
+    best_laps = raw_laps.drop_duplicates(subset= ['Year', 'EventName', 'Driver', 'SessionType'], keep='first')
 
     # obliczanie i implementacja formy w kwalifikacjach
-    qualy_history = raw_laps[raw_laps['SessionType'] == 'Q'][['Year', 'EventName', 'Driver', 'SessionRank']].copy()
+    qualy_history = best_laps[best_laps['SessionType'] == 'Q'][['Year', 'EventName', 'Driver', 'SessionRank', 'RoundNumber']].copy()
+    qualy_history = qualy_history.sort_values(['Year', 'RoundNumber'])
     qualy_history['RecentQForm'] = qualy_history.groupby('Driver')['SessionRank'].transform(
         lambda x: x.shift(1).rolling(window=3, min_periods=1).mean()
     )
     qualy_history = qualy_history[['Year', 'EventName', 'Driver', 'RecentQForm']]
-    raw_laps = pd.merge(raw_laps, qualy_history, on=['Year', 'EventName', 'Driver'], how='left')
+    best_laps = pd.merge(best_laps, qualy_history, on=['Year', 'EventName', 'Driver'], how='left')
+
+    # pivotowanie tabeli
+    pivot_table = best_laps.pivot_table(
+        index= ['Driver', 'TeamName', 'Year', 'EventName', 'IsSprint', 'RecentQForm', 'Track_high_aero', 'Track_high_speed', 'Track_street', 'Track_technical'],
+        columns= ['SessionType'],
+        values= ['SessionRank', 'DeltaToLeader', 'DeltaToTeammate', 'TheoreticalDelta'],
+    )
+    # spłaszczenie kolumn
+    pivot_table.columns = [f'{col[0]}_{col[1]}' for col in pivot_table.columns]
+    # reset indexów
+    pivot_table = pivot_table.reset_index()
+
 
     # wynik w kwalifikacjach rok temu
 
 
     # delta w kwalifikacjach rok temu
+
+
+    # maksymalna prędkość podczas weekendu (speedtrap)
 
 
     # punkty kierowcy z ostatnich 3 wyscigow
@@ -67,9 +90,15 @@ def big_table():
     # średnia temperatura w sesji przed kwalifikacjami (IsSprint == 0 to FP3, IsSprint == 1 oraz Year <= 2023 to FP1, IsSprint == 1 oraz Year >= 2024 to SQ)
 
 
-    return raw_laps
+    return pivot_table
 
 
 if __name__ == "__main__":
     tabela = big_table()
     print("Tabela gotowa")
+
+# test skutecznosci = z sprintami i bez
+
+# opcje przewidywania:
+# 1. przewidywanie zwycięzcy (target is_pole, procentowe szanse dla kazdego)
+# 2. przewidywanie straty do lidera (delty) (target DeltaToLeader_Q, delta dla kazdego)
